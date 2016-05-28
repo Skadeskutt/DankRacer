be2ab36b145773e21a41f0ecdc5ea443e75a6dfb
Jonas Myhr Refseth <jonas@oisann.net>
2016-05-28 14:57:00 +0200

    AItest

---
+++ Assets/_Scripts/AI/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class EnemyAI : MonoBehaviour {
+    public float speed = 80f;
+    public GameObject detectors;
+    public List<RayCastAI> rayCast = new List<RayCastAI>();
+
+    private float verticalSens = 3f;
+
+	void Start() {
+        if(detectors == null)
+            detectors = transform.FindChild("Detectors").gameObject;
+	}
+	
+	void FixedUpdate() {
+        listDetectors();
+
+
+        if(Application.isPlaying) {
+
+
+            float step = (speed) * Time.deltaTime;
+            transform.Translate(Vector3.forward * step);
+
+
+            foreach(RayCastAI ai in rayCast) {
+                RaycastHit hit;
+                if(Physics.Raycast(ai.GetBasePos(), ai.GetDirection(), out hit)) {
+                    if(hit.distance <= ai.GetDirection().magnitude)
+                        Debug.Log("HIT dist: " + hit.distance);
+                }
+            }
+        }
+    }
+
+    private void listDetectors() {
+        rayCast.Clear();
+        foreach(Transform go in detectors.transform) {
+            rayCast.Add(go.GetComponent<RayCastAI>());
+        }
+    }
+
+}
---
+++ Assets/_Scripts/AI/RayCastAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class RayCastAI : MonoBehaviour {
+    public Transform target;
+
+	void Start() {
+	    target = this.transform.FindChild("Target");
+    }
+	
+	public Vector3 GetTargetPos() {
+        return target.position;
+    }
+
+    public Vector3 GetBasePos() {
+        return this.transform.position;
+    }
+
+    public Vector3 GetDirection() {
+        return GetTargetPos() - GetBasePos();
+    }
+}
