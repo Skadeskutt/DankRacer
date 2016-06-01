a11f4c90ccb6faa1a84d7354306e430132544772
Jonas Myhr Refseth <jonas@oisann.net>
2016-06-01 22:51:32 +0200

    Pathfinding

--- Assets/_Scripts/AI/EnemyAI.cs
+++ Assets/_Scripts/AI/EnemyAI.cs
@@ -5,42 +5,82 @@ using System.Collections.Generic;
 [ExecuteInEditMode]
 public class EnemyAI : MonoBehaviour {
     public float speed = 0f;
-    public float speedTarget = 80f;
+    public float speedTarget = 100f;
     public GameObject detectors;
     public PathNode nextNode;
     public List<RayCastAI> rayCast = new List<RayCastAI>();
 
     private float verticalSens = 3f;
+    private int nodeCounter = 0;
+    private Vector3 targetPos;
+    private Vector3 lastNodePos;
 
-	void Start() {
+    void Start() {
         if(detectors == null)
             detectors = transform.FindChild("Detectors").gameObject;
-	}
-	
-	void FixedUpdate() {
+        if(nextNode == null)
+            nextNode = GameObject.Find("Path").transform.GetChild(0).GetComponent<PathNode>();
+        nodeCounter = Random.Range(0, 10000);
+        lastNodePos = transform.position;
+        targetPos = nextNode.transform.position;
+
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        if(targetPos != Vector3.zero) {
+            Gizmos.DrawLine(transform.position, targetPos);
+            Gizmos.DrawSphere(targetPos, 1f);
+        } else {
+            Gizmos.DrawLine(transform.position, transform.position + (Vector3.right * 10f));
+            Gizmos.DrawSphere(transform.position + (Vector3.right * 10f), 1f);
+        }  
+    }
+
+    void FixedUpdate() {
         listDetectors();
 
+        if(transform.position.y <= -10f) {
+            transform.position = lastNodePos;
+            speed = 0f;
+        }
 
         if(Application.isPlaying) {
 
             speed = updatedSpeed(speed);
 
-            if(Vector3.Distance(transform.position, nextNode.transform.position) <= 10f) {
-                nextNode = nextNode.getNextNode();
+            if(Vector3.Distance(transform.position, targetPos) <= 20f) {
+                lastNodePos = nextNode.transform.position;
+                nextNode = nextNode.getNextNode(gameObject.name + "" + nodeCounter);
+                targetPos = nextNode.getRandomPoint(gameObject.name + "" + nodeCounter, transform.position);
                 speed -= 10f;
+                nodeCounter++;
             }
 
             float step = (speed) * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, nextNode.transform.position, step);
+            Vector3 tp = new Vector3(targetPos.x, transform.position.y, targetPos.z);
 
-            transform.LookAt(nextNode.transform);
+            //transform.position = Vector3.MoveTowards(transform.position, tp, step);
+            transform.Translate(Vector3.forward * (speed * Time.deltaTime), Space.Self);
+
+            GameObject lookLocation = new GameObject("Looking at");
+            lookLocation.transform.SetParent(transform);
+            lookLocation.transform.position = tp;
+            //transform.LookAt(lookLocation.transform);
+
+            Vector3 relativePos = tp - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * (speed / 10f) / 2.5f);
+
+
+            Destroy(lookLocation);
 
 
             foreach(RayCastAI ai in rayCast) {
                 RaycastHit hit;
                 if(Physics.Raycast(ai.GetBasePos(), ai.GetDirection(), out hit)) {
-                    if(hit.distance <= ai.GetDirection().magnitude)
-                        Debug.Log("HIT dist: " + hit.distance);
+                    /*if(hit.distance <= ai.GetDirection().magnitude)
+                        Debug.Log("HIT dist: " + hit.distance);*/
                 }
             }
         }
@@ -49,7 +89,7 @@ public class EnemyAI : MonoBehaviour {
     private float updatedSpeed(float last) {
         float newSpeed = last += (last / 100f);
         if(newSpeed == 0f)
-            newSpeed = 1f;
+            newSpeed += Random.Range(1f, 10f);
         return Mathf.Clamp(newSpeed, 0f, speedTarget);
     }
 
--- Assets/_Scripts/Path/PathNode.cs
+++ Assets/_Scripts/Path/PathNode.cs
@@ -2,20 +2,139 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[ExecuteInEditMode]
 public class PathNode : MonoBehaviour {
     public List<PathNode> nextNode = new List<PathNode>();
+    public Color nodeColor = Color.blue;
+
+    private Vector3 forward;
+    private Vector3 back;
+    private List<Color> childColors = new List<Color>();
 
     void Start() {
+        childColors.Add(Color.green);
+        childColors.Add(Color.magenta);
+        childColors.Add(Color.red);
+        childColors.Add(Color.yellow);
+        childColors.Add(Color.white);
+        childColors.Add(Color.grey);
+        childColors.Add(Color.blue);
         if(nextNode.Count <= 0) {
             Debug.LogError("Node needs one or more nodes to point to.", gameObject);
         }
     }
-    
-	void Update () {
-	
-	}
 
-    public PathNode getNextNode() {
-        return nextNode[Random.Range(0, nextNode.Count)];
+    void OnDrawGizmos() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, 5f);
+
+        if(back != null && forward != null) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, forward);
+            Gizmos.DrawLine(transform.position, back);
+        }
+
+        foreach(PathNode pn in nextNode) {
+            Gizmos.color = pn.nodeColor;
+            Gizmos.DrawLine(transform.position, pn.transform.position);
+        }
+    }
+
+    void Update() {
+        RaycastHit fw, bk;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out fw)) {
+            forward = fw.point;
+        }
+        if(Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.forward), out bk)) {
+            back = bk.point;
+        }
+    }
+
+    public Color getRandomColor() {
+        Color result = childColors[Random.Range(0, childColors.Count)];
+        childColors.Remove(result);
+        return result;
+    }
+
+    public Vector3 getRandomPoint(int seed, Vector3 playerPos) {
+        return getRandomPoint(seed.ToString(), playerPos);
+    }
+
+    public Vector3 getRandomPoint(string seed, Vector3 playerPos) {
+        System.Random rand = new System.Random(seed.GetHashCode());
+
+        Vector3 f = transform.InverseTransformPoint(forward);
+        Vector3 p = transform.InverseTransformPoint(transform.position);
+        Vector3 b = transform.InverseTransformPoint(back);
+
+        float howCloseToPrefered = rand.Next(0, 100);
+        float z;
+
+        //Try to cut the corner
+        if(howCloseToPrefered != -1f) {
+            if(Vector3.Distance(playerPos, forward) < Vector3.Distance(playerPos, back)) {
+                if(f.z > p.z) {
+                    z = randomCloseToPrefered(rand, p.z, f.z);
+                } else {
+                    z = randomCloseToPrefered(rand, f.z, p.z);
+                    z -= f.z;
+                }
+            } else {
+                if(b.z > p.z) {
+                    z = randomCloseToPrefered(rand, p.z, b.z);
+                } else {
+                    z = randomCloseToPrefered(rand, b.z, p.z);
+                    z -= b.z;
+                }
+            }
+        } else {
+            if(f.z > b.z) {
+                z = randomCloseToPrefered(rand, b.z, f.z);
+            } else {
+                z = randomCloseToPrefered(rand, f.z, b.z);
+            }
+        }
+        
+        Vector3 result = new Vector3(0f, 0f, z);
+
+        return transform.TransformPoint(result);
+
+    }
+
+    public float randomCloseToPrefered(System.Random rand, float min, float max) {
+        float part = (max - min) / 5;
+        int terning = rand.Next(1, 6);
+        float percent = rand.Next(0, 10000) / 100f;
+        float newMax;
+        float newMin;
+
+        if(percent > 25f) {
+            newMin = min;
+            newMax = min + part;
+        } else if(percent > 12.5f) {
+            newMin = min + (part * 1);
+            newMax = min + (part * 2);
+        } else if(percent > 6.5f) {
+            newMin = min + (part * 2);
+            newMax = min + (part * 3);
+        } else if(percent > 3.125f) {
+            newMin = min + (part * 3);
+            newMax = min + (part * 4);
+        } else {
+            newMin = min + (part * 4);
+            newMax = min + (part * 5);
+        }
+        
+        return (rand.Next(Mathf.RoundToInt(newMin * 100f), Mathf.RoundToInt(newMax * 100f)) / 100f);
+    }
+
+    public PathNode getNextNode(int seed) {
+        return getNextNode(seed.ToString());
+    }
+
+    public PathNode getNextNode(string seed) {
+        System.Random rand = new System.Random(seed.GetHashCode());
+
+        return nextNode[rand.Next(0, nextNode.Count)];
     }
 }
